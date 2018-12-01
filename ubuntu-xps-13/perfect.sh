ecb5e21ff1a48e6020ed77e4d229b41736607d07
codeninja55 <andrew@codeninja55.me>
2018-12-02 03:32:29 +1100

    Additional nvidia and xrandr configs

---
+++ ubuntu-xps-13/perfect.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --primary --mode 3200x1800 --pos 7520x2880 --rotate normal --scale 1x1 \
+--output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2 \
+--output DP-0 --mode 3440x1440 --pos 3840x0 --rotate normal --scale 2x2
