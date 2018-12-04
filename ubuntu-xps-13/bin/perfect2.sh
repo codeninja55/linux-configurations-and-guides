319790a71dbb953dc61dd0f6d83eb86383747e14
Andrew Che <andrew@codeninja55.me>
2018-12-04 16:55:05 +1100

    Added scripts used for xrandr

---
+++ ubuntu-xps-13/bin/perfect2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1-1 --primary --mode 3200x1800 --pos 3840x2880 --rotate normal --output DP-1-2 --off --output USB-C-0 --off --output DP-1-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --mode 3440x1440 --pos 3840x0 --rotate normal
---
+++ ubuntu-xps-13/bin/xrandrsetup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --scale 2x2 --output DP-0 --mode 3440x1440 --pos 3840x0 --scale 2x2 --output eDP-1-1 --mode 3200x1800 --pos 3840x2880 --scale 1x1 --primary
